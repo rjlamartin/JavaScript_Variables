d820ab3c5e0373cb35623895352cd12eef6e0c9a
Reece Martin <reecejmartin19@gmail.com>
2022-10-20 09:58:30 -0400

    modified.js.vars

--- app.js
+++ app.js
@@ -4,7 +4,30 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Reece";
+let lastName = 'Martin';
+var age = 21;
 
 // Exercise 2
+lastName = 'Martin'
+age = 21
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let iCaseSensitive = true;
+
+console.log(`${language} was first release in ${createdYear}.`);
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
